a814cee60e2293e2687f2201c47893f9bb390160
Andrii Hrabovenskyi <ahrabovenskyi@ancestry.com>
2021-07-14 11:43:16 +0300

    improved version with comments

--- src/hello_compojure/handler.clj
+++ src/hello_compojure/handler.clj
@@ -5,35 +5,51 @@
             [clojure.data.json :as json]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+; here we store the language separately for every request
 (def ^:dynamic dynamic-language "en")
+
+(def translations-map (json/read-str (slurp (io/resource (str "public/translations.json"))) :key-fn keyword))
+(def supported-languages (conj (set (map name (keys translations-map))) "en"))
+
+; since language is stored in 'dynamic-language' var
+; we don't need to pass the language as argument to the translation function
+(defn translate [string-to-translate]
+  (if (= dynamic-language "en")
+    string-to-translate
+    (let [language-key (keyword dynamic-language)
+          string-to-translate-key (keyword string-to-translate)
+          translations-map-for-the-language (language-key translations-map)
+          result (string-to-translate-key translations-map-for-the-language)]
+      result)))
+
+; new middleware where we retrieve the language from the header adn store it to 'dynamic-language' var
+(defn save-language-header
+  [handler]
+  (fn [request]
+    (let [headers (:headers request)
+          headers-as-map (zipmap (map keyword (keys headers)) (vals headers))
+          language-header (:language headers-as-map)
+          ;if language is not supported, english is used
+          language-to-use (or (some #(and (= language-header %) %) supported-languages) "en")]
+      ; The whole magic happens here.
+      ; Because of (handler request) is wrapped by binding,
+      ; every request has its own dynamic-language value which can not be overridden by another request.
+      (binding [dynamic-language language-to-use] (handler request)))))
 
 (defn map-keys
   [m f]
   (zipmap (map f (keys m)) (vals m)))
 
-(defn translate [s]
-  ;(let [f (future (Thread/sleep 5000) (println "done") (println "language in future" dynamic-language) 100)] @f)
-  (if (= dynamic-language "en")
-    s
-    (let [translation-as-string (slurp (io/resource (str "public/" dynamic-language "/translation.txt")))
-          translation-map (map-keys (json/read-str translation-as-string) keyword)
-          result ((keyword s) translation-map)]
-      result)))
-
-(defn save-language-header
-  [handler]
-  (fn [request]
-    (let [headers (:headers request)
-          headers-as-map (map-keys headers keyword)
-          language-header (:language headers-as-map "en")]
-      (binding [dynamic-language language-header] (handler request))))
-  )
-
 (defroutes app-routes
-           (GET "/hello" []  (translate "Hello"))
+           ; and this how we perform the translation.
+           ; we just pass the string to 'translate' function as argument
+           (GET "/hello" [] (translate "Hello"))
            (route/not-found "Not Found"))
 
 (def app
   (-> app-routes
+      ; in this middleware, we store language to dynamic-language variable that can be accessed from any place of code
       (wrap-routes save-language-header)
       (wrap-defaults site-defaults)))
+
+
